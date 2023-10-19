aade9cb1bfc083307a8a18b110297cd5131669ef
Per(k) <aupper094@gmail.com>
2023-10-19 16:16:07 +0700

    Template Engine

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -8,15 +8,19 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(morgan("combined")); // .use is the method to use the package, combined is the format of the log in the console
-
 // express.static is the method to use the static file,
 // __dirname is the path of the current file, "/public" is the folder name of the static file
 app.use(express.static(path.join(__dirname, "/public/"))); 
 
+app.set('views', __dirname + '/src/views');// .set is the method to set the view engine
+app.set('view engine', 'ejs');
+
 app.get("/", (req,res) => { // "/" is the root path, .get is the method to get the data from the server
     // req is the request from the client
-    res.send("Hello World my app"); // res is the response from the server
+    res.render('index', {username: "PerTRA",  cutomers:["a","b","c"]}); // res is the response from the server
 }) 
+
+
 
 app.listen(PORT, () => { // .listen is the method to listen to the port 3002 and run the server on that port 
     debug(`Server is running on port ${chalk.green(PORT)}`);
--- README.md
+++ README.md
@@ -7,4 +7,33 @@
 > **_Diffent_**  
 > require in CommonJS (Old Gen) **_|_** import in ES6 (New Gen)
 
-## npm script
+## npm scripts
+
+package.json
+
+    "scripts": {
+        "start": "set DEGUP-DEBUG=app & node ./ExpressJS/app.js", // add script
+    	"debug": "set DEGUP-DEBUG=* & node node ./ExpressJS/app.js", // add script
+        "test": "echo \"Error: no test specified\" && exit 1"
+    },
+
+## nodemon
+
+    npm i nodemon
+
+package.json
+
+    //write script
+    "nodemonConfig": {
+        "restartable": "rs",
+    	"delay": 2000
+    }
+    "scripts": { "start": "set DEGUP-DEBUG=app & nodemon ./ExpressJS/app.js", // edit node	-> nodemon
+    			"debug": "set DEGUP-DEBUG=* & nodemon ./ExpressJS/app.js",
+    			"test": "echo \"Error: no test specified\" && exit 1" },
+
+## Template Engine
+
+### EJS
+
+    npm install ejs
